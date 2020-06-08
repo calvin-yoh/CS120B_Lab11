82f439a131568cfc8c0edfaed75c1fb723fcc566
Calvin Yoh <flashfire340@gmail.com>
2020-06-07 20:34:38 -0700

    update

--- turnin/cyoh001_lab11_part2.c
+++ turnin/cyoh001_lab11_part2.c
@@ -47,7 +47,7 @@ int main(void) {
     const unsigned short numTasks = sizeof(tasks) / sizeof(task*);
 
     task1.state = Display;
-    task1.period = 10;
+    task1.period = 100;
     task1.elapsedTime = task1.period;
     task1.TickFct = &DisplaySentance_Tick;
 
