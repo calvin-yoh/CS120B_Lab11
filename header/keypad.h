cedba80dbe22710afb8c9903717735debd97f5fb
Calvin Yoh <flashfire340@gmail.com>
2020-06-07 20:45:38 -0700

    Update keypad.h

--- header/keypad.h
+++ header/keypad.h
@@ -22,8 +22,8 @@
 #include <bit.h>
 
 // Keypad Setup Values
-#define KEYPADPORT PORTA
-#define KEYPADPIN  PINA
+#define KEYPADPORT PORTC
+#define KEYPADPIN  PINC
 #define ROW1 0
 #define ROW2 1
 #define ROW3 2
